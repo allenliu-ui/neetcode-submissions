46648ef1063c2d8c6af24164ed79144e46e7c793
Allen Liu <allenliu042007@gmail.com>
2026-06-09 22:40:26 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.2
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "hello"
+print(type(variable))
+variable = [3,4,5]
+print(type(variable))
