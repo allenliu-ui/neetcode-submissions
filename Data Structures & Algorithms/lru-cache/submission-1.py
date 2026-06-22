7bcff7e655373458e2973e0cd2afccabf03453e0
Allen Liu <allenliu042007@gmail.com>
2026-06-22 16:43:44 -0700

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+
+        self.front = Node(0, 0)
+        self.back = Node(0, 0)
+        self.front.next = self.back
+        self.back.prev = self.front
+
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def insert(self, node):
+        prev = self.back.prev 
+        nxt = self.back
+        prev.next = node
+        nxt.prev = node
+        node.next = nxt
+        node.prev = prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+        if len(self.cache) > self.capacity:
+            least = self.front.next
+            self.remove(least)
+            del self.cache[least.key]
+
+
