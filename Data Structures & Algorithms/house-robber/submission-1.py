7412e3d72f26e64c74e187609df7ad75e9ae5c62
Allen Liu <allenliu042007@gmail.com>
2026-07-03 12:24:50 -0700

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+        def helper(i):
+            if i < 0:
+                return 0
+            if i == 0:
+                return nums[0]
+            if i in memo:
+                return memo[i]
+            memo[i] = max(nums[i] + helper(i-2), helper(i-1))
+            return memo[i]
+        return helper(len(nums)-1)
