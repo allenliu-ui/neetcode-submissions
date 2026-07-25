bb7471e20eff721ff4d28437147db4db6db917a6
Allen Liu <allenliu042007@gmail.com>
2026-07-25 00:53:07 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        minPrice = prices[0]
+        for price in prices:
+            if price < minPrice:
+                minPrice = price
+            else:
+                profit = price - minPrice
+                maxProfit = max(maxProfit, profit)
+        return maxProfit
